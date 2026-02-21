cd29796e65d49d06994e6c7e8e5cad71e1e550f2
unknown <ilhamrifai166@gmail.com>
2026-02-21 19:10:46 +0700

    perbahan image dan judul

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import "./Navbar.css";
 import logoImg from "../../images/logo.png";
+import  logoPerpus from "../../images/logoperpus.png"
 import {HiOutlineMenuAlt3} from "react-icons/hi";
 
 const Navbar = () => {
@@ -13,8 +14,8 @@ const Navbar = () => {
       <div className='container navbar-content flex'>
         <div className='brand-and-toggler flex flex-sb'>
           <Link to = "/" className='navbar-brand flex'>
-            <img src = {logoImg} alt = "site logo" />
-            <span className='text-uppercase fw-7 fs-24 ls-1'>bookhub</span>
+            <img src = {logoPerpus} alt = "site logo" />
+            <span className='text-uppercase fw-7 fs-24 ls-1'>PerpusKabBogor</span>
           </Link>
           <button type = "button" className='navbar-toggler-btn' onClick={handleNavbar}>
             <HiOutlineMenuAlt3 size = {35} style = {{
